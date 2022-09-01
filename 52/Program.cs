ecf209aaa3a0086a92f793c80100386b788edf7e
Vitaliy Novozhilov <don@vitalino.ru>
2022-09-01 16:52:39 +0300

    ex 52

---
+++ 52/Program.cs
@@ -0,0 +1,47 @@
+﻿//Доработал метод вывода массива с семинара, сделал обход по столбцам и ввёл переменную для суммирования значений в столбцах
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+PrintSum(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintSum(int[,] inArray)
+{   
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {   
+        int sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum += inArray[i, j];
+        }
+        Console.WriteLine($"Сумма чисел в столбце {j} = {sum}");
+    }
+}
